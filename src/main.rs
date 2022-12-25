9130088a665eb2bcf8b332ab8e0c9b80a3858a93
adrgs <albastroiudragos@gmail.com>
2022-12-25 15:29:43 +0100

    Implement PING

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{net::TcpListener, io::{Write, BufRead}};
+
+fn handle_request(request: &str) -> String {
+    if request.starts_with("PING") {
+        return "+PONG\r\n".to_string();
+    }
+    return "-ERR unknown command\r\n".to_string();
+}
+
+fn handle_connection(stream: std::net::TcpStream) {
+    let mut reader = std::io::BufReader::new(&stream);
+    let mut writer = std::io::BufWriter::new(&stream);
+
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer).unwrap();
+    let response = handle_request(&buffer);
+    writer.write(response.as_bytes()).unwrap();
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,13 +27,6 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        handle_connection(stream.unwrap())
     }
 }
